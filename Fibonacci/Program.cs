229b425a5d2bf180708ccb32e4414a071e32a7d7
Aleksandr <komptver@gmail.com>
2023-01-08 23:10:07 +0300

    added Fibonacci

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,14 @@
+﻿// f(1) = 1
+// f(2) = 1
+// f(n) = f(n-1) + f(n-2)
+
+double Fibonacci(int n) // можно взять int
+{
+    if(n==1 || n== 2) return 1;// если n=1 или n=2, то возвращаем 1
+    else return Fibonacci(n-1) + Fibonacci(n-2);
+}
+
+for (int i = 1; i < 40; i++)
+{
+    Console.WriteLine(Fibonacci(i));
+}
